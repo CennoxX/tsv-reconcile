da4bd55f7ce0e3e5525f0d813d418aee91bcc7fe
CennoxX <cesar.bernard@gmx.de>
2018-11-28 18:10:58 +0100

    committed DDKFMs edits

--- reconcile.c
+++ reconcile.c
@@ -10,8 +10,6 @@ char * rowname_1 = NULL;
 char * filename_2 = NULL;
 char * rowname_2 = NULL;
 char * output = NULL;
-char * lines[0];
-int numberoflines = 0;
 int idrowtoget = 0;
 
 void loadarguments(int argi, char **argv)
@@ -21,7 +19,7 @@ void loadarguments(int argi, char **argv)
 		printf("format: %s <input file 1> <input row 1> <input file 2> <input row 2> <output file>\n",argv[0]);
 		exit(0);
 	}
-	
+
 	filename_1 = strdup(argv[1]);
 	rowname_1 = strdup(argv[2]);
 	filename_2 = strdup(argv[3]);
@@ -38,8 +36,7 @@ void test_loadarguments(int argi, char **argv)
 	output = strdup("output.tsv");
 }
 
-
-void fileExists(char *filename)
+void filecheck(char *filename)
 {
 	FILE * filepointer;
 	if(filepointer = fopen(filename, "r"))
@@ -48,10 +45,11 @@ void fileExists(char *filename)
 	}
 	else
 	{
-		printf("%s existiert nicht\n", filename);
+		printf("%s existiert nicht oder der Zugriff auf die Datei wird verweigert.\n", filename);
 		exit(0);
 	}
 }
+
 int getrownumber(char * filename,char * rowname)
 {
 	char * puffer = NULL;
@@ -59,17 +57,17 @@ int getrownumber(char * filename,char * rowname)
 	char * temp = NULL;
 	temp = (char *)malloc(sizeof(char)*IDLENGTH);
 	puffer = (char *)malloc(sizeof(char)*LINELENGTH);
-	
+
 	FILE * filepointer;
 	filepointer = fopen(filename,"r");
-	
+
 	if (fgets(puffer,LINELENGTH,filepointer)!=NULL)
 	{
 		char * eol = rindex(puffer,'\n');
 		if (eol != NULL) *eol='\0';
 		eol = rindex(puffer,'\r');
 		if (eol != NULL) *eol='\0';
-		
+
 		temp = strtok(puffer, "\t");
 		while(strcmp(temp,rowname) != 0)
 		{
@@ -82,22 +80,46 @@ int getrownumber(char * filename,char * rowname)
 	return rowtoget;
 }
 
-char ** getlinesarray(char * filename)
+char ** getlinesarray(char * filename, int *numberoflines, int sourcerow)
 {
 	char * puffer = NULL;
-	numberoflines = 0;
+	char ** lines = NULL;		  
+	*numberoflines = 0;
 	FILE * filepointer;
 	filepointer = fopen(filename,"r");
-	puffer = (char *)malloc(sizeof(char)*LINELENGTH);
-	fgets(puffer,LINELENGTH,filepointer);
-	while (fgets(puffer,LINELENGTH,filepointer)!=NULL)
+	puffer = (char *) malloc(sizeof(char)*LINELENGTH);
+	fgets(puffer,LINELENGTH,filepointer);//exclude the first line
+
+	while (fgets(puffer,LINELENGTH,filepointer) != NULL)
 	{
 		char * eol = rindex(puffer,'\n');
 		if (eol != NULL) *eol='\0';
 		eol = rindex(puffer,'\r');
 		if (eol != NULL) *eol='\0';
-		
-		lines[numberoflines++]=strdup(puffer);
+		*numberoflines = *numberoflines + 1;
+		lines = realloc(lines, sizeof(char) * LINELENGTH * *numberoflines);//erweitert char ** immer dynamisch um ein Element
+		char * tokTemp = strtok(puffer, "\t");
+		int i = 0;
+		char *row = malloc(sizeof(char) * LINELENGTH);
+		strcpy(row, "");
+		char *imdbId = malloc(sizeof(char) * LINELENGTH);
+		while(tokTemp != NULL)
+		{
+			if(i == sourcerow)
+			{
+				strcpy(imdbId, tokTemp);
+			}
+			else 
+			{
+				strcat(row, tokTemp);
+				strcat(row, "\t");
+			}
+			tokTemp = strtok(NULL, "\t");
+			i++;
+		}
+		strcat(imdbId, "\t");
+		strcat(imdbId, row);
+		lines[*numberoflines - 1] = strdup(imdbId);
 	}
 	free(puffer);
 	fclose(filepointer);
@@ -116,7 +138,6 @@ char * getidfromfile(const void *p)
 	for(i=0; i < idrowtoget; i++) temp = strtok(NULL, "\t");
 	return temp;
 }
-
 int cmpids(const void *p1, const void *p2)
 {
 	return strcmp(getidfromfile(p1), getidfromfile(p2));
@@ -129,17 +150,15 @@ void Free()
 	free(rowname_1);
 	free(rowname_2);
 	free(output);
-	while(numberoflines > 0) free(lines[numberoflines--]);
 }
 
 int main(int argi, char **argv)
 {
 	int i = 0;
-	char ** lines_1 = NULL;
-	loadarguments(argi, argv);//comment out for testing
-	//test_loadarguments(argi, argv);
-	printf("\e[1;1H\e[2J");
+	if(strcmp(strdup(argv[1]),"--test") == 0) test_loadarguments(argi, argv);
+	else loadarguments(argi, argv);
 
+	printf("\e[1;1H\e[2J");//clear terminal
 	printf("+---------------------------------------------------+\n");
 	printf("|                     Reconcile                     |\n");
 	printf("|                                                   |\n");
@@ -148,22 +167,31 @@ int main(int argi, char **argv)
 	printf("|                                                   |\n");
 	printf("+---------------------------------------------------+\n");
 
-	fileExists(filename_1);
+	filecheck(filename_1);
 	int rowtoget_1 = getrownumber(filename_1, rowname_1);
 	printf("index of row from %s to get: %d\n", filename_1, rowtoget_1);
-	lines_1 = getlinesarray(filename_1);
+	int numberoflines_1 = 0;
+	char ** list_1 = getlinesarray(filename_1, &numberoflines_1, rowtoget_1);
 	idrowtoget = rowtoget_1;
-	printf("\nFile %s:\n",filename_1);
-	for(i=0; i < numberoflines; i++) printf("%s\n",lines_1[i]);
-	printf("\nFile sorted by IDs:\n");
-	qsort(lines_1, numberoflines, sizeof(char*), cmpids);	
-	for(i=0; i < numberoflines; i++) printf("%s\n",lines_1[i]);
-	printf("\n");
-	
-	fileExists(filename_2);	
-	int rowtoget_2 = getrownumber(filename_2, rowname_2);
-	printf("index of row from %s to get: %d\n", filename_2, rowtoget_2);
+	printf("\nIDs from %s:\n", filename_1);
 
+	for(i=0; i < numberoflines_1; i++)
+	{
+		printf("%s\n",list_1[i]);
+	}
+
+	filecheck(filename_2);
+	int rowtoget_2 = getrownumber(filename_2, rowname_2);
+	printf("index of row %s to get: %d\n", filename_2, rowtoget_2);
+	int numberoflines_2 = 0;
+	char ** list_2 = getlinesarray(filename_2, &numberoflines_2, rowtoget_2);
+	idrowtoget = rowtoget_2;
+	printf("\nIDs from %s\n", filename_2);
+
+	for(i = 0 ; i < numberoflines_2 ; i++)
+	{
+		printf("%s\n", list_2[i]);
+	}
 	Free();
 	return 0;
 }
