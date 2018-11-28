3c6edf95d296e6a9a0cd2b9f0d6a2c9f68a4dfeb
CennoxX <cesar.bernard@gmx.de>
2018-11-28 22:21:56 +0100

    fix form

--- reconcile.c
+++ reconcile.c
@@ -19,13 +19,13 @@ void printtime(){
 }
 
 void clear(){
-    #if defined(__linux__) || defined(__unix__) || defined(__APPLE__)
-        system("clear");
-    #endif
-
-    #if defined(_WIN32) || defined(_WIN64)
-        system("cls");
-    #endif
+	#if defined(__linux__) || defined(__unix__) || defined(__APPLE__)
+		system("clear");
+	#endif
+	
+	#if defined(_WIN32) || defined(_WIN64)
+		system("cls");
+	#endif
 }
 
 void loadarguments(int argi, char **argv)
